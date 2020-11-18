122c2657d0a1e00bb0bbb18545b769d5c5dd9690
LlortBored <71751176+LlortBored@users.noreply.github.com>
2020-11-18 15:56:00 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
 })
-radio.setGroup(5)
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+})
 basic.forever(function () {
 	
 })
